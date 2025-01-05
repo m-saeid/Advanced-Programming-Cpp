f1d03a367cba6aa511e3b1177fd0b8191bc5256c
Mohammad Saeid <89332212+m-saeid@users.noreply.github.com>
2025-01-05 06:25:35 -0800

    Basic concepts and Vector

---
+++ 01_Basic concepts/04_CalculateAge.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int birthYear;
+    int year = 1403;
+    cout << "Enter your birth year: ";
+    cin >> birthYear;
+    cout << "U R " << year-birthYear << " years old.";
+}
